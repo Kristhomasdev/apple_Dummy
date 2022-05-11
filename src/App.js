b7d340db81f119ef1eec597143ab61bb71d7dd62
Krislucas <kaadan7@gmail.com>
2022-05-11 10:07:49 +0530

    First commit {General} includes header and  a corousel ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+//imort bootstra in main Alication.js file
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./compos/header/header";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { f179} from "@fortawesome/free-solid-svg-icons";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Carrousel from "./compos/carousel/Carousel";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header/> 
+    <Carrousel/>
+      
     </div>
   );
 }
